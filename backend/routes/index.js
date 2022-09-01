7ded01e5111d091e154434d65f88652fe370bdd6
labor-day <shaffer.scott@yahoo.com>
2022-09-01 12:16:40 -0600

    setup api router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
 
+//use api router for all routes beginning with /api
+router.use('/api', apiRouter);
+
+//test route
 router.get('/hello/world', function(req, res) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
   res.send('Hello World!');
+});
+
+// Add a XSRF-TOKEN cookies
+router.get("/api/csrf/restore", (req, res) => {
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    'XSRF-Token': csrfToken
+  });
 });
 
 module.exports = router;
