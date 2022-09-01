f7ad2f3c3fcbaf91f88f2be0a688ae20301ee882
labor-day <shaffer.scott@yahoo.com>
2022-09-01 15:20:47 -0600

    added login route handler

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,16 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+
 const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
 const { restoreUser } = require('../../utils/auth.js');
 const { requireAuth } = require('../../utils/auth.js');
 
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
+
+/******** TEST ROUTES BELOW ********/
 router.post('/test', function(req, res) {
   res.json({requestBody: req.body});
 });
