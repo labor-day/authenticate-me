f4d9735918f95d81de22fc63e964914950f8519e
labor-day <shaffer.scott@yahoo.com>
2022-09-01 13:29:49 -0600

    added error handlers

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,8 @@ const helmet = require('helmet'); //security middleware
 const cookieParser = require('cookie-parser'); //parse cookies
 const { environment } = require('./config'); //get env variables from config folder
 const isProduction = environment === 'production'; //check if environment is production
-const routes = require('./routes');
+const routes = require('./routes'); //import route handlers
+const { ValidationError } = require('sequelize'); // catch sequelize errors
 
 /********* INITIALIZE SERVER *********/
 const app = express();
@@ -41,8 +42,39 @@ app.use(
   })
 );
 
-//connect routers to server
+// Connect routers to server
 app.use(routes);
+
+// Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = ["The requested resource couldn't be found."];
+  err.status = 404;
+  next(err);
+});
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = 'Validation error';
+  }
+  next(err);
+});
+
+// Last error handler formats all errors before returning response
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
+  });
+});
 
 /******* EXPORT THE APP *******/
 module.exports = app;
